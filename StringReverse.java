da165ee087390d27587753eda74290c2c31818e9
HarishkumarRamanathan <harishkumar.rcse@ksrce.ac.in>
2016-07-22 11:41:37 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ReverseString {
+
+	public static void main(String[] args) {
+String str="Javaprogrammer";
+StringBuilder builder=new StringBuilder(str);
+System.out.println(builder.reverse());
+}
+}
